72b82dacd117a096a9c73c3e90da11b40762c8fb
Dominic Black <dpblack96@gmail.com>
2022-07-15 10:55:43 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector('#modal')
 
 // Your JavaScript code goes here!
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  errorModal.classList.add('hidden')
+  clickListener()
+})
+
+function hideError() {
+  errorModal.classList.add('hidden')
+}
+
+function clickListener() {
+  const likeArr = document.querySelectorAll('.like-glyph')
+  document.addEventListener('click', (e) => {
+    if(e.target.classList[0] === 'like-glyph'){
+      mimicServerCall()
+        .then(resp => {
+          const activated = e.target.classList.contains('activated-heart');
+          if (activated) {
+            e.target.classList.remove('activated-heart')
+            e.target.innerHTML = EMPTY_HEART
+          }
+          else {
+            e.target.classList.add('activated-heart')
+            e.target.innerHTML = FULL_HEART
+          }
+        })
+        .catch(error => {
+          setTimeout(() => {
+            hideError
+          }, 3000)
+        })
+    }
+  })
+}
+ 
 
 
 //------------------------------------------------------------------------------
